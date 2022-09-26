09865733897d6b933931e83910a937d314c28b55
Josh Pierce <joshua.alan.pierce@gmail.com>
2022-09-25 19:14:20 -0600

    Add event listener for the difficult game

--- src/main.js
+++ src/main.js
@@ -7,26 +7,29 @@ var computer = new Player("Computer", "🤖");
 //-------------------Query Selectors---------------//
 
 var classicGame = document.querySelector(".classic-game");
+var difficultGame = document.querySelector(".difficult-game");
 var playerInfo = document.querySelector(".player-info");
 var computerInfo = document.querySelector(".computer-info");
 var playerAvatar = document.querySelector("avatar");
 var homeScreen = document.querySelector(".home-view");
 var classicScreen = document.querySelector(".classic-game-view");
+var difficultScreen = document.querySelector(".difficult-game-view");
 var selectRock = document.querySelector(".rock-image");
 var selectPaper = document.querySelector(".paper-image");
 var selectScissors = document.querySelector(".scissors-image");
 
 //--------------------Event Listeners--------------//
 
-window.addEventListener("load", generatePlayerCard);
+window.addEventListener("load", updatePlayerCard);
 classicGame.addEventListener("click", classicGameView);
+difficultGame.addEventListener("click", difficultGameView)
 selectRock.addEventListener("click", rockClassic);
-//selectPaper.addEventListener("click", paperClassic);
-//selectScissors.addEventListener("click", scissorsClassic)
+selectPaper.addEventListener("click", paperClassic);
+selectScissors.addEventListener("click", scissorsClassic);
 
 //--------------------Event Handlers-----------------//
 
-function generatePlayerCard() {
+function updatePlayerCard() {
   playerInfo.innerHTML = `
     <div class="player-name">
       <p class="avatar">${human.avatar}</p>
@@ -52,14 +55,34 @@ function classicGameView(){
   removeHidden(classicScreen)
 };
 
+function difficultGameView() {
+  displayHidden(homeScreen)
+  removeHidden(difficultScreen)
+}
+
 function rockClassic() {
   var newGame = new Game (human, computer)
   human.selection = classicOptions[0];
   computer.selection = (classicOptions[randomSelection(classicOptions)])
   newGame.checkForWin(human.selection)
-  generatePlayerCard()
-}
+  updatePlayerCard()
+};
 
+function paperClassic() {
+  var newGame = new Game (human, computer)
+  human.selection = classicOptions[1];
+  computer.selection = (classicOptions[randomSelection(classicOptions)])
+  newGame.checkForWin(human.selection)
+  updatePlayerCard()
+};
+
+function scissorsClassic() {
+  var newGame = new Game (human, computer)
+  human.selection = classicOptions[2];
+  computer.selection = (classicOptions[randomSelection(classicOptions)])
+  newGame.checkForWin(human.selection)
+  updatePlayerCard()
+};
 //--------------- Misc Functions---------------//
 
 function displayHidden(element){
